8127acedb387f2e1aa54c15b92e8e2ca4dea9ec5
kijoo <kjyoon0125@gmail.com>
2026-05-18 20:15:45 +0900

    changed k to 2

--- search.py
+++ search.py
@@ -1,8 +1,11 @@
+from vectorstore import build_vectorstore
+
 def search(vectorstore, query):
-    retriever = vectorstore.as_retriever()
+    retriever = vectorstore.as_retriever(search_kwargs={"k":2})
     results = retriever.invoke(query)
     
     for r in results:
         print(r.metadata["path"])
         print(r.page_content)
         print("-" * 50)
+
