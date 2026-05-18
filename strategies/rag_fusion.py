69e821a1834f47e0440b39e0b5ff343b692f93c9
kijoo <kjyoon0125@gmail.com>
2026-05-19 03:08:53 +0900

    filtering out docs that exceed the similarity dist threshold to retrieve only the relevant ones

--- strategies/rag_fusion.py
+++ strategies/rag_fusion.py
@@ -1,6 +1,8 @@
 from chains.multiquery import MultiQueryGenerator
 from collections import defaultdict
 
+SIMILARITY_DIST_THRESHOLD = 1.55
+
 class RAGFusionStrategy:
     def __init__(self, llm, retriever):
         self.llm = llm
@@ -14,9 +16,15 @@ class RAGFusionStrategy:
         docs = {}
         
         for q in generated_queries:
-            retrieved_docs = self.retriever.invoke(q)
+            retrieved_docs = self.retriever.vectorstore.similarity_search_with_score(
+                q,
+                k=5
+            )
             
-            for rank,doc in enumerate(retrieved_docs):
+            for rank, (doc,score) in enumerate(retrieved_docs):
+                print(score)
+                if score < SIMILARITY_DIST_THRESHOLD:
+                    continue
                 key = (
                     doc.metadata["path"] + doc.page_content
                 )
@@ -27,5 +35,6 @@ class RAGFusionStrategy:
         res = []
         for key,score in sorted(scores.items(), key=lambda x: x[1], reverse=True):
             res.append(docs[key])
+        
         return res  
         
