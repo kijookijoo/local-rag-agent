3377abca007f790898d90e075afe0b2c8fbc9fee
kijoo <kjyoon0125@gmail.com>
2026-05-19 02:41:16 +0900

    made according changes to the refactoring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from rag import RAGAgent
 from pathlib import Path
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-import chains
+from strategies.rag_fusion import RAGFusionStrategy
 import os
 import getpass
 
@@ -16,25 +16,29 @@ if not os.getenv("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")    
 
 def main():
+    llm = ChatOpenAI(
+        model_name= os.getenv("OPENAI_MODEL_NAME", "gpt-4o-mini"), 
+        temperature=0
+    )
     cwd = Path(os.getcwd())
+    
     docs = read_directory(cwd)
+    
     langchain_docs = to_langchain_docs(docs)
     split_docs = chunk_docs(langchain_docs)
     vectorstore = VectorStore()
     vectorstore.index_documents(split_docs)
     print("Indexing done!")
     
-    # ChatOpenAI automatically picks up OPENAI_API_KEY from .env
-    agent = RAGAgent(vectorstore, 
-                     ChatOpenAI(
-                         model_name= os.getenv("OPENAI_MODEL_NAME", "gpt-4o-mini"), 
-                         temperature=0
-                         )
-                     )
+    retriever = vectorstore.as_retriever(k=4)
+    
+    strategy = RAGFusionStrategy(llm, retriever)
+    # ChatOpenAI automatically picks up OPENAI_API_KEY from .env    
+    agent = RAGAgent(strategy, llm)
     
     while True:
         query = input("Enter query :")
-        result = vectorstore.search(query)
+        result = retriever.invoke(query)
         
         if result:
             print("Retrieved Documents:")
