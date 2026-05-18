7f2f5415ab26d307e6ad377d615598170fea35e5
kijoo <kjyoon0125@gmail.com>
2026-05-19 01:32:28 +0900

    implemented MultiQueryGenerator to generate multiple versions of the given prompt

---
+++ chains/multiquery.py
@@ -0,0 +1,25 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+class MultiQueryGenerator:
+    def __init__(self, llm):
+        prompt = ChatPromptTemplate.from_template(
+            """
+            You are a helpful assistant that generates multiple semantic search queries.
+
+            Generate 4 alternative search queries related to:
+
+            {question}
+
+            Return only the queries, one per line.
+            """ 
+        )
+        
+        self.chain = (
+            prompt 
+            | llm
+            | StrOutputParser()
+            | (lambda x: x.split("\n"))
+        )
+        
