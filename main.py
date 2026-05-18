e09b138a49aa54aed03af24e508a22f64e182e5b
kijoo <kjyoon0125@gmail.com>
2026-05-19 00:05:58 +0900

    implemented REPL for persistence in coversation

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 from ingest import read_directory
-from chunker import chunk_docs,to_langchain_docs
-from vectorstore import build_vectorstore
+from chunker import chunk_docs
+from ingest import to_langchain_docs
+from vectorstore import VectorStore
+from rag import RAGAgent
 from pathlib import Path
-from search import search
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+import getpass
+
+load_dotenv()
+
+if not os.getenv("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")    
 
 def main():
-    directory = Path(input("Enter directory: "))
-    docs = read_directory(directory)
+    cwd = Path(os.getcwd())
+    docs = read_directory(cwd)
     langchain_docs = to_langchain_docs(docs)
     split_docs = chunk_docs(langchain_docs)
-    vectorstore = build_vectorstore(split_docs)
+    vectorstore = VectorStore()
+    vectorstore.index_documents(split_docs)
     print("Indexing done!")
-    query = input("Enter query :")
-    search(vectorstore, query)
     
+    # ChatOpenAI automatically picks up OPENAI_API_KEY from .env
+    agent = RAGAgent(vectorstore, 
+                     ChatOpenAI(
+                         model_name= os.getenv("OPENAI_MODEL_NAME", "gpt-4o-mini"), 
+                         temperature=0
+                         )
+                     )
+    
+    while True:
+        query = input("Enter query :")
+        result = vectorstore.search(query)
+        
+        if result:
+            print("Retrieved Documents:")
+            print("\n")
+            
+        for doc in result:
+            print(doc.page_content)
+            print("\n")
+            print("-" * 20)
+            
+        response = agent.ask(query)
+        print("-" * 20)
+        print(response)
+        
+# RAG Chain
+# Retrieval -> fed into context window along with prompt -> generation
 if __name__ == "__main__":
     main()
     
