92447c78f8c2c3cf6515f6393a96607598ca8224
kijoo <kjyoon0125@gmail.com>
2026-05-18 20:15:22 +0900

    adjusted function call order in main to allow querying

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from ingest import read_directory
 from chunker import chunk_docs,to_langchain_docs
 from vectorstore import build_vectorstore
 from pathlib import Path
+from search import search
 
 def main():
     directory = Path(input("Enter directory: "))
     docs = read_directory(directory)
     langchain_docs = to_langchain_docs(docs)
     split_docs = chunk_docs(langchain_docs)
-    chromadb = build_vectorstore(split_docs)
+    vectorstore = build_vectorstore(split_docs)
     print("Indexing done!")
-
-main()
+    query = input("Enter query :")
+    search(vectorstore, query)
+    
+if __name__ == "__main__":
+    main()
     
     
     
